dbeb494b1b9e66a8cba0a431ad63585f24388451
crhilditch20 <crhilditch20@gmail.com>
2017-01-20 11:32:23 +0000

    create RecordStore object and test properties

--- record_store.js
+++ record_store.js
@@ -1,3 +1,7 @@
-
+var RecordStore = function(name, city){
+  this.name = name;
+  this.city = city;
+  this.inventory = [];
+};
 
 module.exports = RecordStore;
--- specs/recordstore_spec.js
+++ specs/recordstore_spec.js
@@ -1,3 +1,24 @@
-// var assert = require('assert');
-// var Record = require('../record');
-// var RecordStore = require('../record_store');
+var assert = require('assert');
+var Record = require('../record');
+var RecordStore = require('../record_store');
+
+describe('record', function(){
+  var record1; 
+  var record2;
+  var recordStore;
+
+  beforeEach(function(){
+    record1 = new Record("David Bowie", "Blackstar", 6.00);
+    record2 = new Record("Imelda May", "Tribal", 5.00);
+    recordStore = new RecordStore("Record Cave", "Edinburgh");
+  });
+
+  it('should have a name', function(){
+    assert.equal("Record Cave", recordStore.name);
+  });
+
+  it('should have a city', function(){
+    assert.equal("Edinburgh", recordStore.city);
+  });
+
+});
