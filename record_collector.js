6acd07b33e58a9b204a3beb6e1148b329f7b59be
crhilditch20 <crhilditch20@gmail.com>
2017-01-20 15:03:37 +0000

    add record collector with properties and test

--- record_collector.js
+++ record_collector.js
@@ -1,3 +1,7 @@
-
+var RecordCollector = function(name, cash){
+  this.name = name;
+  this.cash = cash;
+  this.collection = [];
+};
 
 module.exports = RecordCollector;
--- specs/recordcollector_spec.js
+++ specs/recordcollector_spec.js
@@ -1,4 +1,31 @@
-// var assert = require('assert');
-// var RecordCollector = require('../record_collector');
-// var Record = require('../record');
-// var RecordStore = require('../record_store');
+var assert = require('assert');
+var RecordCollector = require('../record_collector');
+var Record = require('../record');
+var RecordStore = require('../record_store');
+
+describe('record collector', function(){
+  var record1;
+  var record2;
+  var recordStore;
+  var recordCollector;
+
+  beforeEach(function(){
+    record1 = new Record("David Bowie", "Blackstar", 6.00);
+    record2 = new Record("Imelda May", "Tribal", 5.00);
+    recordStore = new RecordStore("Record Cave", "Edinburgh");
+    recordCollector = new RecordCollector("Jeff", 30.00);
+  });
+
+  it('should have a name', function(){
+    assert.equal("Jeff", recordCollector.name);
+  });
+
+  it('should have some cash', function(){
+    assert.equal(30.00, recordCollector.cash);
+  });
+
+  it('should start with empty collection', function(){
+    assert.deepEqual([], recordCollector.collection);
+  });
+
+});
